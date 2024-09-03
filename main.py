543388d7fa6d26203629d68b5414e88819c3b3db
shiv <shivkumar.bhise.eng@gmail.com>
2024-09-04 01:58:38 +0530

    add random module

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+import random
 '''
 1 for snake
 -1 for water 
 0 for gun
 '''
-computer = -1
+computer = random.choice([-1, 0, 1])
 youstr = input("Enter your choice: ")
 youDict = { "s":1, "w":-1, "g":0 }
+reverseDict = { 1:"Snake", -1:"Water", 0:"Gun" }
+
 you = youDict[youstr]
 
-if(computer == -1 and you ==1):
-    print("YOU WON!")
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
 
-elif(computer == -1 and you ==0):
-    print("YOU LOSE!")
+if(computer == you):
+    print("IT'S A DRAW")
 
-elif(computer == 1 and you ==-1):
-    print("YOU LOSE!")
-
-elif(computer == 1 and you ==0):
-    print("YOU WON!")        
-
-elif(computer == 0 and you ==-1):
-    print("YOU WIN!")
-
-elif(computer == 0 and you ==1):
-    print("YOU LOSE!")
 else:
-    print("SOMETHING WENT WRONG!")
+
+    if(computer == -1 and you ==1):
+        print("YOU WON!")
+
+    elif(computer == -1 and you ==0):
+        print("YOU LOSE!")
+
+    elif(computer == 1 and you ==-1):
+        print("YOU LOSE!")
+
+    elif(computer == 1 and you ==0):
+        print("YOU WON!")        
+
+    elif(computer == 0 and you ==-1):
+        print("YOU WIN!")
+
+    elif(computer == 0 and you ==1):
+        print("YOU LOSE!")
+    else:
+        print("SOMETHING WENT WRONG!")
+
